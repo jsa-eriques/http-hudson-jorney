3378c9f8472ee2474a845c9e4eb8fe4ddc388049
Hudson M <121588128+Frylet@users.noreply.github.com>
2024-06-02 22:30:47 -0300

    alguns exercicios concluídos

--- second-project/src/challenges.js
+++ second-project/src/challenges.js
@@ -1,11 +1,31 @@
 // Desafio 1
+const a = 23
+const b = 20
+
+console.log( a > b && b < a);
 
 // Desafio 2
 
+let pv = 10;
+let sv = 13; 
+let tv = 7;
+
+let p = (pv + sv + tv)/2;
+
+let area = Math.sqrt(p * ((p - pv) * (p - sv) * (p - tv)));
+console.log(area);
+
 // Desafio 3
+
+let nome = "Hudson Moreira Oliveira";
+let r = nome.split(' ');
+console.log(r)
 
 // Desafio 4
 
+const newname = "Hudson"
+let newvar = (`Olá, ${newname}`)
+console.log(newvar)
 // Desafio 5
 
 // Desafio 6
